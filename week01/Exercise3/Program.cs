9cdb5f68bdf29ea6aff1c910493819099257cad6
Mozart Soares <mozartarmstrong@gmail.com>
2025-01-11 23:00:48 -0300

    Completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome top the random number game !");
+        Random random = new();
+        int magicNumber = random.Next(1, 100);
+        bool won = false;
+        while (won == false)
+        {
+            Console.WriteLine("What is your guess?");
+            bool validInput = false;
+            int number = 0;
+            while (!validInput)
+            {
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+            if (number == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                won = true;
+            }
+            else if (number < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        
+        
+
     }
 }
