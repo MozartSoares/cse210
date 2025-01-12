181f76642f53fbc81c254be4e1e9bb8e72dfa7e3
Mozart Soares <mozartarmstrong@gmail.com>
2025-01-12 16:28:38 -0300

    Completed Learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using Resumes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new()
+        {
+            _title = "Software developer",
+            _company = "Microsoft",
+            _endYear = 2024,
+            _startYear = 2015
+        };
+
+        Job job2 = new()
+        {
+            _title = "CEO",
+            _company = "Meta",
+            _startYear = 1998,
+            _endYear = 2025
+        };
+
+        Resume myResume = new()
+        {
+            _name = "Allison Rose"
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
