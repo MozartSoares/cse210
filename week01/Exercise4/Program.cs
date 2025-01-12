678821a9b2fe304491b04aa9db093712c2822783
Mozart Soares <mozartarmstrong@gmail.com>
2025-01-11 23:09:47 -0300

    Completed exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = [];
+        int userInput = 999;
+        while (userInput !=0)
+        {
+            try
+            {
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput != 0) {
+            numbers.Add(userInput);
+            }
+            }
+            catch (FormatException)
+            {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        if (numbers.Count > 0)
+        {
+            float average = (float)sum / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+            if (number > max)
+            {
+                max = number;
+            }
+            }
+            Console.WriteLine($"The largest number is: {max}");
+        }
+
     }
 }
